0b6e854a52eaace02f690546319fde85656e513e
Ruofan Wu <ruofanw@umich.edu>
2025-08-07 03:58:13 +0000

    megatron+perseus

---
+++ tests/perseus/run_profiling.sh
@@ -0,0 +1,6 @@
+for frequency in $(seq 1620 -15 210); do
+    nvidia-smi -i 0,1,2,3 --lock-gpu-clocks=${frequency},${frequency}
+    python megatron_gpt_pretraining.py
+done
+
+# ZEUS_PFO_SCHEDULER_ARGS='{"solution_path": "/workspaces/Kareus/tests/perseus/nemo_experiments/megatron_llama_3_2_1b/perseus_results_70/freqs_pipeline_01346.py"}' uvicorn zeus.optimizer.pipeline_frequency.server.router:app --port 7787
