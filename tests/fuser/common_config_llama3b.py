9fbda62c392dfe7c7e7b1fdef3c2fdbd96b84b50
Ruofan Wu <ruofanw@umich.edu>
2025-09-13 03:21:26 +0000

    update scripts lamdalab

--- tests/fuser/common_config_llama3b.py
+++ tests/fuser/common_config_llama3b.py
@@ -30,8 +30,8 @@ class FuserTestConfig:
 
     DEFAULT_STAGES = 2
     DEFAULT_NUM_MICROBATCHES = 8
-    num_layers_in_first_pipeline_stage = 6
-    num_layers_in_last_pipeline_stage = 6
+    num_layers_in_first_pipeline_stage = 15
+    num_layers_in_last_pipeline_stage = 13
     
     # Default Bayesian Optimization parameters
     BO_DEFAULT_N_INIT = 48
