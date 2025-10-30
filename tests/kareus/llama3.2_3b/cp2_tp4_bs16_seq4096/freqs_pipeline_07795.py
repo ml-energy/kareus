c85a03941b1850d923a661b451a9b272578e0b1f
Ruofan Wu <ruofanw@umich.edu>
2025-10-30 05:20:43 +0000

    update 2nodes scripts

---
+++ tests/kareus/llama3.2_3b/cp2_tp4_bs16_seq4096/freqs_pipeline_07795.py
@@ -0,0 +1,7 @@
+[
+[('forward', 1380), ('forward', 900), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 1020), ('backward', 1380)],
+[('forward', 1380), ('backward', 1380), ('forward', 1230), ('backward', 1260), ('forward', 1230), ('backward', 1260), ('forward', 1230), ('backward', 1260), ('forward', 1230), ('backward', 1260), ('forward', 1230), ('backward', 1260), ('forward', 1230), ('backward', 1260), ('forward', 1380), ('backward', 1380)],
+]
+# Iteration 7795 cost change: 1.3228146259039448
+# Iteration 7795 total cost: 4095.4874692044796
+# Iteration 7795 total cost with P2P: 5697.5674692044795
--- tests/perseus/megatron_gpt_pretraining.py
+++ tests/perseus/megatron_gpt_pretraining.py
@@ -36,7 +36,7 @@ torch._dynamo.config.suppress_errors = True
 mp.set_start_method("spawn", force=True)
 
 
-@hydra_runner(config_path="conf", config_name="megatron_llama_3_2_1b_config_half")
+@hydra_runner(config_path="conf", config_name="megatron_llama_3_2_3b_config_2nodes")
 def main(cfg) -> None:
     logging.info("\n\n************** Experiment configuration ***********")
     logging.info(f'\n{OmegaConf.to_yaml(cfg)}')
