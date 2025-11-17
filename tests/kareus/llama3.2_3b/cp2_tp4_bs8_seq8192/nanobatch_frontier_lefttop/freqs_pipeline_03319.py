098354e4594869b46cd66f6c18a1faaa3899f63d
Ruofan Wu <ruofanw@umich.edu>
2025-11-17 07:20:36 +0000

    more nanobatch for 8 8192

---
+++ tests/kareus/llama3.2_3b/cp2_tp4_bs8_seq8192/nanobatch_frontier_lefttop/freqs_pipeline_03319.py
@@ -0,0 +1,7 @@
+[
+[('forward', 1410), ('forward', 1020), ('backward', 1290), ('forward', 1290), ('backward', 1290), ('forward', 1290), ('backward', 1290), ('forward', 1290), ('backward', 1290), ('forward', 1290), ('backward', 1290), ('forward', 1290), ('backward', 1290), ('forward', 1290), ('backward', 990), ('backward', 1410)],
+[('forward', 1380), ('backward', 1410), ('forward', 1230), ('backward', 1200), ('forward', 1230), ('backward', 1200), ('forward', 1230), ('backward', 1200), ('forward', 1230), ('backward', 1200), ('forward', 1230), ('backward', 1200), ('forward', 1230), ('backward', 1200), ('forward', 1320), ('backward', 1410)],
+]
+# Iteration 3319 cost change: 1.4956744142567402
+# Iteration 3319 total cost: 3257.031984866015
+# Iteration 3319 total cost with P2P: 4795.361984866015
---
+++ tests/kareus/llama3.2_3b/cp2_tp4_bs8_seq8192/nanobatch_frontier_lefttop/freqs_pipeline_03565.py
@@ -0,0 +1,7 @@
+[
+[('forward', 1410), ('forward', 1020), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 1380), ('forward', 1380), ('backward', 990), ('backward', 1410)],
+[('forward', 1410), ('backward', 1410), ('forward', 1290), ('backward', 1290), ('forward', 1290), ('backward', 1290), ('forward', 1290), ('backward', 1290), ('forward', 1290), ('backward', 1260), ('forward', 1290), ('backward', 1260), ('forward', 1290), ('backward', 1260), ('forward', 1410), ('backward', 1410)],
+]
+# Iteration 3565 cost change: 2.066202402945791
+# Iteration 3565 total cost: 3691.830708952658
+# Iteration 3565 total cost with P2P: 5188.340708952658
